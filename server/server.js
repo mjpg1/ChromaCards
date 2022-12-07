a616f6d53043bedaddf6c326947104e998bd84d5
Mia <mjpgussen@gmail.comn>
2022-12-07 11:17:05 -0500

    established basic folder structure and configured webpack for dev mode

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// require in other routers
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('client'));
+
+// define route handlers
+
+app.use((req, res) => res.status(404).send('Page not found'));
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
+
+module.exports = app;
