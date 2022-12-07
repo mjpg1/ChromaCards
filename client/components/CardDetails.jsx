272f4c47c71d1f43067d6a7e28cc56c5853b113e
Mia <mjpgussen@gmail.comn>
2022-12-07 14:50:39 -0500

    added basic progress bar component

--- client/components/CardDetails.jsx
+++ client/components/CardDetails.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import ProgressBar from './ProgressBar.jsx';
 
 const CardDetails = ({ color, gray }) => {
-  const style = {
-    width: '350px',
-    // boxShadow: 'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px',
-    boxShadow: 'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
-    borderRadius: '1.2em',
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '15px',
+    // textAlign: 'center',
     zIndex: '1',
     position: 'fixed',
     top: '50%',
@@ -13,9 +16,20 @@ const CardDetails = ({ color, gray }) => {
     transform: 'translate(-50%, -50%)'
   }
 
+  const cardStyle = {
+    width: '350px',
+    boxShadow: 'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
+    borderRadius: '1.2em',
+  }
+
   const type = gray ? 'gray' : 'color';
 
-  return <img src={`client/assets/${type}-cards/${color}.png`} style={style} />;
+  return (
+    <div style={containerStyle}>
+      <img src={`client/assets/${type}-cards/${color}.png`} style={cardStyle} />
+      <ProgressBar color={color} progress={50} />
+    </div>
+  );
 };
 
 export default CardDetails;
---
+++ client/components/ProgressBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ProgressBar = ({ color, progress }) => {
+  const outlineStyle = {
+    width: '400px',
+    height: '15px',
+    border: '2px solid black'
+  };
+
+  const fillStyle = {
+    backgroundColor: 'red',
+    height: '15px',
+    width: `${progress}%`
+  }
+
+  return (
+    <div style={outlineStyle}>
+      <div style={fillStyle}></div>
+    </div>
+  )
+};
+
+export default ProgressBar;
