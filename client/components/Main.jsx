6c730570638e4c1122596b29fbbb32b2211789e9
Mia <mjpgussen@gmail.com>
2023-03-20 16:28:15 -0400

    switch from react hooks to redux for ease of access across entrypoints AND separate server reqs into services dir

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-axios.defaults.withCredentials = true;
+import { useDispatch, useSelector } from 'react-redux';
 
-import colors from '../../colors';
+import userService from '../services/user';
+import { setUser } from '../reducers/userSlice';
+import { setColorProgress } from '../reducers/colorProgressSlice';
+
 import CardsContainer from './CardsContainer.jsx';
 import LoginSignupModal from './LoginSignupModal.jsx';
 import ProgressModal from './ProgressModal.jsx';
@@ -10,76 +12,44 @@ import Menu from './Menu.jsx';
 
 /* TODO
  ** - keep user from opening multiple modals/card details at once
- ** - in order to be able to access colors from chrome extension, move colors to db
  ** - instead of signin button, have default page when user isn't logged in include a modal
  **   - introducing new player to game, inviting them to login
  **   - sign in with google button
  */
 
-// convert 2D array of color names and codes into array of objects with progress set to 0 (when no user signed in)
-const initialColorProgress = colors.map(([color, code]) => ({
-  color,
-  code,
-  progress: 0,
-}));
-
 const Main = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
+  const colorProgress = useSelector(state => state.colorProgress);
+
   // show appropriate modals when requested by user
   const [loggingIn, setLoggingIn] = useState(false);
-  const [signingUp, setSigningUp] = useState(false);
   const [checkingProgress, setCheckingProgress] = useState(false);
-
-  // store data about logged-in user and their color progress (or default if no user logged in)
-  const [user, setUser] = useState(null);
-  const [colorProgress, setColorProgress] = useState(initialColorProgress);
 
   // check for logged in user (based on session cookie)
   useEffect(() => {
-    const getUser = async () => {
-      try {
-        const response = await axios.get('http://localhost:3000/users/current');
-        const currentUser = response.data;
-        if (currentUser) setUser(currentUser);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getUser();
+    userService.getUser()
+      .then(currentUser => {
+        if (currentUser) dispatch(setUser(currentUser));
+      })
+      .catch(err => console.log(err))
   }, [])
 
 
   // set color progress based on whether or not a user is logged in
   useEffect(() => {
-    if (user) {
-      setColorProgress(
-        initialColorProgress.map((colorInfo) => {
-          const { color, progress } = colorInfo;
-          return color in user.progress
-            ? { ...colorInfo, progress: user.progress[color] }
-            : colorInfo;
-        })
-      );
-    } else {
-      setColorProgress(initialColorProgress);
-    }
+    if (user) dispatch(setColorProgress(user.progress))
   }, [user]);
 
   // close modal if a user clicks 'cancel'
-  const handleCancel = () => {
-    setLoggingIn(false);
-    setSigningUp(false);
-  };
+  const handleCancel = () => setLoggingIn(false);
 
   // send login request to server (by way of google oauth) and set user data in state accordingly
   const handleLogin = async (res) => {
     const idToken = res.credential;
     try {
-      const { data } = await axios({
-        method: 'post',
-        url: 'http://localhost:3000/users/login',
-        headers: { 'Authorization': `Bearer ${idToken}` }
-      });
-      setUser(data);
+      const newUser = await userService.loginUser(idToken);
+      dispatch(setUser(newUser));
       setLoggingIn(false);
     } catch (err) {
       console.log(err);
@@ -90,9 +60,9 @@ const Main = () => {
   // else if a user wants to sign in, open the login modal by setting 'loggingIn' to true
   const handleSignInOut = async () => {
     if (user) {
-      setUser(null);
+      dispatch(setUser(null));
       try {
-        await axios.post('http://localhost:3000/users/logout');
+        await userService.logoutUser();
       } catch (err) {
         console.log(err);
       }
@@ -110,7 +80,7 @@ const Main = () => {
         handleSignInOut={handleSignInOut}
       />
       <CardsContainer colorProgress={colorProgress} />
-      {(loggingIn || signingUp) && (
+      {loggingIn && (
         <LoginSignupModal
           loggingIn={loggingIn}
           handleCancel={handleCancel}
--- client/index.js
+++ client/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App.jsx';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <GoogleOAuthProvider clientId={process.env.CLIENT_ID}>
-    <App />
-  </GoogleOAuthProvider>
+  <Provider store={store}>
+    <GoogleOAuthProvider clientId={process.env.CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </Provider>
 );
 
 /** (a couple resources on google oauth with extensions)
